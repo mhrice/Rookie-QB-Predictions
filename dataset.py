c6dd398b7b0fe29105a710eb2b817a97556593ea
Matthew Rice <mhrice@ucsd.edu>
2022-06-19 17:51:41 -0700

    Removed uneeded files

--- dataset.py
+++
@@ -1,8 +0,0 @@
-from fastai.tabular.all import *
-dls = TabularDataLoaders.from_csv("combined.csv", path=".", 
-                                   cont_names=["Cmp", "Att", "Yds", "TD", "Int", "Y/G", "Sk"],
-                                   y_names=["Cmp2", "Att2", "Yds2", "TD2", "Int2", "Y/G2", "Sk2"],
-                                   procs=[FillMissing, Normalize]
-                                   )
-
-dls.show_batch()
